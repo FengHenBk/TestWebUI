804a414c943befaae78dc3014563aeb0a00da290
FengHen <blnk_0425@163.com>
2024-03-07 22:46:05 +0800

    Pytest使用参数化传递

--- L1/test_ceshiren.py
+++ L1/test_ceshiren.py
@@ -1,5 +1,6 @@
 import time
 
+import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
@@ -15,8 +16,8 @@ class TestCeshiren:
         # 关闭浏览器
         self.webdriver.quit()
 
-
-    def test_search_selenium(self):
+    @pytest.mark.parametrize("search_value", ["Selenium", "Appium", "面试"])
+    def test_search_selenium(self,search_value):
         """
         测试 https://ceshiren.com/ 的搜索功能
 
@@ -30,51 +31,52 @@ class TestCeshiren:
         self.webdriver.maximize_window()
         self.webdriver.implicitly_wait(3)
         # 2.输入框输入
-        self.webdriver.find_element(By.ID, 'ember15').send_keys("Selenium")
+        # self.webdriver.find_element(By.ID, 'ember15').send_keys("Selenium")
+        self.webdriver.find_element(By.ID, 'ember15').send_keys(search_value)
         # 3.点击搜索按钮
         self.webdriver.find_element(By.XPATH, '//*[@id="ember13"]/div[2]/div[1]/button/span').click()
         # 断言
         text = self.webdriver.find_element(By.CSS_SELECTOR, '.topic-title').text
-        assert "Selenium" in text
+        assert search_value in text
 
-    def test_search_appium(self):
-        """
-        测试 https://ceshiren.com/ 的搜索功能
-
-        1.打开 https://ceshiren.com/
-        2.输入框输入
-        3.点击搜索
-        :return:
-        """
-        # 1.进入 https://ceshiren.com/search页面
-        self.webdriver.get('https://ceshiren.com/search')
-        self.webdriver.maximize_window()
-        self.webdriver.implicitly_wait(3)
-        # 2.输入框输入
-        self.webdriver.find_element(By.ID, 'ember15').send_keys("Appium")
-        # 3.点击搜索按钮
-        self.webdriver.find_element(By.XPATH, '//*[@id="ember13"]/div[2]/div[1]/button/span').click()
-        # 断言
-        text = self.webdriver.find_element(By.CSS_SELECTOR, '.topic-title').text
-        assert "Appium" in text
-
-    def test_search_mianshi(self):
-        """
-        测试 https://ceshiren.com/ 的搜索功能
-
-        1.打开 https://ceshiren.com/
-        2.输入框输入
-        3.点击搜索
-        :return:
-        """
-        # 1.进入 https://ceshiren.com/search页面
-        self.webdriver.get('https://ceshiren.com/search')
-        self.webdriver.maximize_window()
-        self.webdriver.implicitly_wait(3)
-        # 2.输入框输入
-        self.webdriver.find_element(By.ID, 'ember15').send_keys("面试")
-        # 3.点击搜索按钮
-        self.webdriver.find_element(By.XPATH, '//*[@id="ember13"]/div[2]/div[1]/button/span').click()
-        # 断言
-        text = self.webdriver.find_element(By.CSS_SELECTOR, '.topic-title').text
-        assert "面试" in text
+    # def test_search_appium(self):
+    #     """
+    #     测试 https://ceshiren.com/ 的搜索功能
+    #
+    #     1.打开 https://ceshiren.com/
+    #     2.输入框输入
+    #     3.点击搜索
+    #     :return:
+    #     """
+    #     # 1.进入 https://ceshiren.com/search页面
+    #     self.webdriver.get('https://ceshiren.com/search')
+    #     self.webdriver.maximize_window()
+    #     self.webdriver.implicitly_wait(3)
+    #     # 2.输入框输入
+    #     self.webdriver.find_element(By.ID, 'ember15').send_keys("Appium")
+    #     # 3.点击搜索按钮
+    #     self.webdriver.find_element(By.XPATH, '//*[@id="ember13"]/div[2]/div[1]/button/span').click()
+    #     # 断言
+    #     text = self.webdriver.find_element(By.CSS_SELECTOR, '.topic-title').text
+    #     assert "Appium" in text
+    #
+    # def test_search_mianshi(self):
+    #     """
+    #     测试 https://ceshiren.com/ 的搜索功能
+    #
+    #     1.打开 https://ceshiren.com/
+    #     2.输入框输入
+    #     3.点击搜索
+    #     :return:
+    #     """
+    #     # 1.进入 https://ceshiren.com/search页面
+    #     self.webdriver.get('https://ceshiren.com/search')
+    #     self.webdriver.maximize_window()
+    #     self.webdriver.implicitly_wait(3)
+    #     # 2.输入框输入
+    #     self.webdriver.find_element(By.ID, 'ember15').send_keys("面试")
+    #     # 3.点击搜索按钮
+    #     self.webdriver.find_element(By.XPATH, '//*[@id="ember13"]/div[2]/div[1]/button/span').click()
+    #     # 断言
+    #     text = self.webdriver.find_element(By.CSS_SELECTOR, '.topic-title').text
+    #     assert "面试" in text
